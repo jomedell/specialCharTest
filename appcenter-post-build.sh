98cc50624b416758d08b2285c2b7ce0c538756d0
Jorge Medellin <jorge.medellin@microsoft.com>
2018-04-23 13:48:56 -0500

    feat(Build Scripts): Post-build script
    #5 comment:
    
    • Install npm and CLI

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -10,6 +10,8 @@ appName="VSAC-Organization/VSACiOS"
 devices="VSAC-Organization/iphoneset"
 testSeries="ScriptTest"
 
- appcenter test run uitest --app $appName --devices $devices --app-path $APPCENTER_OUTPUT_DIRECTORY/*.apk --test-series $testSeries --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/VSACUITests/bin/Debug --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/*/tools --token 6157c7acffeaef8f17ee67d8952e70aaeb6f2fa4 --async
+npm install -g appcenter-cli
+
+appcenter test run uitest --app $appName --devices $devices --app-path $APPCENTER_OUTPUT_DIRECTORY/*.apk --test-series $testSeries --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/VSACUITests/bin/Debug --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/*/tools --token 6157c7acffeaef8f17ee67d8952e70aaeb6f2fa4 --async
 
 #0984302984320948320948
