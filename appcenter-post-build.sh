56c32650ffc65c4910224fe9f3d9384e958206bf
Jorge Medellin <jorge.medellin@microsoft.com>
2018-04-23 13:35:56 -0500

    feat(Build Scripts): Post-build script
    #1 comment:
    
    • Update script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -6,11 +6,10 @@
 #  Created by Jorge Medellin on 4/23/18.
 #  Copyright © 2018 Microsoft. All rights reserved.
 
-appName = "VSAC-Organization/VSACiOS"
-devices = "VSAC-Organization/iphoneset"
-testSeries = "ScriptTest"
+appName="VSAC-Organization/VSACiOS"
+devices="VSAC-Organization/iphoneset"
+testSeries="ScriptTest"
 
-npm install -g appcenter-cli
-appcenter test run uitest --app "VSAC-Organization/VSACiOS" --devices "VSAC-Organization/iphoneset" --app-path $APPCENTER_OUTPUT_DIRECTORY/*.apk --test-series "ScriptTest" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/VSACUITests/bin/Debug --token 6157c7acffeaef8f17ee67d8952e70aaeb6f2fa4 --async
+ appcenter test run uitest --app $appName --devices $devices --app-path $APPCENTER_OUTPUT_DIRECTORY/*.apk --test-series $testSeries --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/VSACUITests/bin/Debug --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/*/tools --token 6157c7acffeaef8f17ee67d8952e70aaeb6f2fa4 --async
 
 #0984302984320948320948
