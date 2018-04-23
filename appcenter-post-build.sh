4f6147116349ee4a679c8f077d5008a787ff5bae
Jorge Medellin <jorge.medellin@microsoft.com>
2018-04-23 13:16:29 -0500

    feat(Build Scripts): Post-build script
    #5 comment:
    
    • Update script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -10,6 +10,7 @@ appName = "VSAC-Organization/VSACiOS"
 devices = "VSAC-Organization/iphoneset"
 testSeries = "ScriptTest"
 
- appcenter test run uitest --app $appName --devices $devices --app-path $APPCENTER_OUTPUT_DIRECTORY/VSACiOS.ipa --test-series $testSeries --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/test/VSACUITests/bin/Debug --token 6157c7acffeaef8f17ee67d8952e70aaeb6f2fa4 --async
+npm install -g appcenter-cli
+appcenter test run uitest --app "VSAC-Organization/VSACiOS" --devices "VSAC-Organization/iphoneset" --app-path $APPCENTER_OUTPUT_DIRECTORY/*.apk --test-series "ScriptTest" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/VSACUITests/bin/Debug --token 6157c7acffeaef8f17ee67d8952e70aaeb6f2fa4 --async
 
 #0984302984320948320948
