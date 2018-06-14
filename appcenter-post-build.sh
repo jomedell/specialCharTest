6ad33bad0cc6d15ec491cf6ae66fe906486ce6d0
Jorge Medellin <jorge.medellin@microsoft.com>
2018-06-14 17:20:22 -0500

    feat(Build Scripts): Post-build script
    #5 comment:
    
    • Update script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -12,16 +12,16 @@ testSeries="ScriptTest"
 
 npm install -g appcenter-cli
 
-#xcrun xcodebuild build-for-testing \
-#-configuration Debug \
-#-workspace $APPCENTER_SOURCE_DIRECTORY/VSAC.xcworkspace \
-#-sdk iphoneos \
-#-scheme VSAC \
-#-derivedDataPath $APPCENTER_SOURCE_DIRECTORY/Test
+xcrun xcodebuild build-for-testing \
+-configuration Debug \
+-workspace $APPCENTER_SOURCE_DIRECTORY/VSAC.xcworkspace \
+-sdk iphoneos \
+-scheme VSAC \
+-derivedDataPath $APPCENTER_SOURCE_DIRECTORY/Test
 
 appcenter login --token 6157c7acffeaef8f17ee67d8952e70aaeb6f2fa4
 
-appcenter test run xcuitest --app $appName --devices $devices --app-path $APPCENTER_OUTPUT_DIRECTORY/*.ipa --test-series $testSeries --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/Test
+appcenter test run xcuitest --app $appName --devices $devices --app-path $APPCENTER_OUTPUT_DIRECTORY/*.ipa --test-series $testSeries --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/Test/Build/Products/Debug-iphoneos
 
 
 #--uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/*/tools
