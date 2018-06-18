555f97cd199b855516b78d07fe3d07d3c4924b9a
Jorge Medellin <jorge.medellin@microsoft.com>
2018-06-18 13:33:30 -0500

    feat(Build Scripts): Post-build script
    #5 comment:
    
    • Update script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -34,11 +34,12 @@ echo "Build UI Test"
 
 #rm -rf DerivedData
 xcrun xcodebuild build-for-testing \
+-allowProvisioningUpdates
 -configuration Debug \
 -workspace $APPCENTER_SOURCE_DIRECTORY/VSAC.xcworkspace \
 -sdk iphoneos \
 -scheme VSAC \
--derivedDataPath $APPCENTER_SOURCE_DIRECTORY/TestBundle
+-derivedDataPath DerivedData
 #-derivedDataPath $APPCENTER_SOURCE_DIRECTORY/Test
 
 
@@ -47,7 +48,7 @@ echo ""
 echo "Run UI test CLI command"
 appcenter login --token
 
-appcenter test run xcuitest --app $appName --devices $devices --app-path $APPCENTER_OUTPUT_DIRECTORY/*.ipa --test-series $testSeries --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/TestBundle/Build/Products/Debug-iphoneos
+appcenter test run xcuitest --app $appName --devices $devices --app-path $APPCENTER_OUTPUT_DIRECTORY/*.ipa --test-series $testSeries --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/DerivedData/Build/Products/Debug-iphoneos
 
 echo "*******************************"
 echo "Post Build Script Complete"
