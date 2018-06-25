bece34743c0d67960f233806e14709c539a5e43f
Jorge Medellin <jorge.medellin@microsoft.com>
2018-06-25 16:54:06 -0500

    feat(Build Scripts): Post-build script
    #5 comment:
    
    • Update script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -21,140 +21,140 @@ echo "******************************************************"
 echo "Origianl CustomVariable"
 echo $CustomVariable
 echo "CustomVariable update"
-$CustomVariable="changedVariable"
+CustomVariable="changedVariable"
 echo $CustomVariable
 echo "Finished updating variable"
 
-appCenterAPItoken=$AppCenterToken
-appName="VSAC-Organization/VSACiOS"
-devices="VSAC-Organization/iphoneset"
-testSeries="ScriptTest"
-
-echo ""
-echo "Start XCUITest"
-echo "App Name: $appName"
-echo "Device Set: $devices"
-echo "Test Series: $testSeries"
-echo ""
-
-echo "Run npm command"
-
-npm install -g appcenter-cli
-
-echo ""
-
-echo "Build UI Test"
-
-rm -rf DerivedData
-xcrun xcodebuild build-for-testing \
--configuration Debug \
--workspace $APPCENTER_SOURCE_DIRECTORY/VSAC.xcworkspace \
--sdk iphoneos \
--scheme VSAC \
--derivedDataPath DerivedData
-
-
-echo ""
-
-echo "Run UI test CLI command"
-appcenter login --token appCenterAPItoken
-
-appcenter test run xcuitest --app $appName --devices $devices --test-series $testSeries --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/DerivedData/Build/Products/Debug-iphoneos
-
-#--app-path $APPCENTER_OUTPUT_DIRECTORY/*.ipa
-
-echo "*******************************"
-echo "Post Build Script Complete"
-echo "*******************************"
-
-#--uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/*/tools
-
-#Xamarin.UITest
-
-#!/bin/sh
-
-#  appcenter-post-build.sh
-#  VSAC
+#appCenterAPItoken=$AppCenterToken
+#appName="VSAC-Organization/VSACiOS"
+#devices="VSAC-Organization/iphoneset"
+#testSeries="ScriptTest"
 #
-#  Created by Jorge Medellin on 4/23/18.
-#  Copyright © 2018 Microsoft. All rights reserved.
-
-#!/usr/bin/env bash
+#echo ""
+#echo "Start XCUITest"
+#echo "App Name: $appName"
+#echo "Device Set: $devices"
+#echo "Test Series: $testSeries"
+#echo ""
 #
-# For Xamarin, run all NUnit test projects that have "Test" in the name.
-# The script will build, run and display the results in the build logs.
-
-
-set -e
-
-echo "******************************************************"
-echo "Post Build Script"
-echo "******************************************************"
-
-appCenterAPItoken=$AppCenterToken
-appName="VSAC-Organization/VSACiOS"
-devices="VSAC-Organization/iphoneset"
-testSeries="ScriptTest"
-
-echo ""
-echo "Start XCUITest"
-echo "App Name: $appName"
-echo "Device Set: $devices"
-echo "Test Series: $testSeries"
-echo ""
-
-echo "Run npm command"
-
-npm install -g appcenter-cli
-
-echo ""
-
-echo "Build UI Test"
-
-rm -rf DerivedData
-xcrun xcodebuild build-for-testing \
--configuration Debug \
--workspace $APPCENTER_SOURCE_DIRECTORY/VSAC.xcworkspace \
--sdk iphoneos \
--scheme VSAC \
--derivedDataPath DerivedData
-
-
-echo ""
-
-echo "list files in APPCENTER_OUTPUT_DIRECTORY"
-ls $APPCENTER_OUTPUT_DIRECTORY
-
-echo "list files in DerivedData SOURCE_DIRECTORY"
-ls $APPCENTER_SOURCE_DIRECTORY/DerivedData
-
-echo "list files in DerivedData Debug-iphoneeos"
-ls $APPCENTER_SOURCE_DIRECTORY/DerivedData/Build/Products/Debug-iphoneos
-
-echo "Run UI test CLI command"
-appcenter login --token appCenterAPItoken
-
-appcenter test run xcuitest --app $appName --devices $devices --test-series $testSeries --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/DerivedData/Build/Products/Debug-iphoneos
-
-#--app-path $APPCENTER_OUTPUT_DIRECTORY/*.ipa
-
-echo "*******************************"
-echo "Post Build Script Complete"
-echo "*******************************"
-
-#--uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/*/tools
-
-
-######### UI Tests (Android)
-
-#echo "Found Xamarin.UITest project:"
-#find $APPCENTER_SOURCE_DIRECTORY -regex '.*UITests.*\.csproj' -exec echo {} \;
-#echo
-#echo "Building Xamarin.UITest project:"
-#find $APPCENTER_SOURCE_DIRECTORY -regex '.*UITests.*\.csproj' -exec msbuild {} \;
-#echo
-#echo "Compiled projects to run NUnit tests:"
-#find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*UITests.*\.dll' -exec echo {} \;
-#echo
-
-
+#echo "Run npm command"
+#
+#npm install -g appcenter-cli
+#
+#echo ""
+#
+#echo "Build UI Test"
+#
+#rm -rf DerivedData
+#xcrun xcodebuild build-for-testing \
+#-configuration Debug \
+#-workspace $APPCENTER_SOURCE_DIRECTORY/VSAC.xcworkspace \
+#-sdk iphoneos \
+#-scheme VSAC \
+#-derivedDataPath DerivedData
+#
+#
+#echo ""
+#
+#echo "Run UI test CLI command"
+#appcenter login --token appCenterAPItoken
+#
+#appcenter test run xcuitest --app $appName --devices $devices --test-series $testSeries --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/DerivedData/Build/Products/Debug-iphoneos
+#
+##--app-path $APPCENTER_OUTPUT_DIRECTORY/*.ipa
+#
+#echo "*******************************"
+#echo "Post Build Script Complete"
+#echo "*******************************"
+#
+##--uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/*/tools
+#
+##Xamarin.UITest
+#
+##!/bin/sh
+#
+##  appcenter-post-build.sh
+##  VSAC
+##
+##  Created by Jorge Medellin on 4/23/18.
+##  Copyright © 2018 Microsoft. All rights reserved.
+#
+##!/usr/bin/env bash
+##
+## For Xamarin, run all NUnit test projects that have "Test" in the name.
+## The script will build, run and display the results in the build logs.
+#
+#
+#set -e
+#
+#echo "******************************************************"
+#echo "Post Build Script"
+#echo "******************************************************"
+#
+#appCenterAPItoken=$AppCenterToken
+#appName="VSAC-Organization/VSACiOS"
+#devices="VSAC-Organization/iphoneset"
+#testSeries="ScriptTest"
+#
+#echo ""
+#echo "Start XCUITest"
+#echo "App Name: $appName"
+#echo "Device Set: $devices"
+#echo "Test Series: $testSeries"
+#echo ""
+#
+#echo "Run npm command"
+#
+#npm install -g appcenter-cli
+#
+#echo ""
+#
+#echo "Build UI Test"
+#
+#rm -rf DerivedData
+#xcrun xcodebuild build-for-testing \
+#-configuration Debug \
+#-workspace $APPCENTER_SOURCE_DIRECTORY/VSAC.xcworkspace \
+#-sdk iphoneos \
+#-scheme VSAC \
+#-derivedDataPath DerivedData
+#
+#
+#echo ""
+#
+#echo "list files in APPCENTER_OUTPUT_DIRECTORY"
+#ls $APPCENTER_OUTPUT_DIRECTORY
+#
+#echo "list files in DerivedData SOURCE_DIRECTORY"
+#ls $APPCENTER_SOURCE_DIRECTORY/DerivedData
+#
+#echo "list files in DerivedData Debug-iphoneeos"
+#ls $APPCENTER_SOURCE_DIRECTORY/DerivedData/Build/Products/Debug-iphoneos
+#
+#echo "Run UI test CLI command"
+#appcenter login --token appCenterAPItoken
+#
+#appcenter test run xcuitest --app $appName --devices $devices --test-series $testSeries --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/DerivedData/Build/Products/Debug-iphoneos
+#
+##--app-path $APPCENTER_OUTPUT_DIRECTORY/*.ipa
+#
+#echo "*******************************"
+#echo "Post Build Script Complete"
+#echo "*******************************"
+#
+##--uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/*/tools
+#
+#
+########## UI Tests (Android)
+#
+##echo "Found Xamarin.UITest project:"
+##find $APPCENTER_SOURCE_DIRECTORY -regex '.*UITests.*\.csproj' -exec echo {} \;
+##echo
+##echo "Building Xamarin.UITest project:"
+##find $APPCENTER_SOURCE_DIRECTORY -regex '.*UITests.*\.csproj' -exec msbuild {} \;
+##echo
+##echo "Compiled projects to run NUnit tests:"
+##find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*UITests.*\.dll' -exec echo {} \;
+##echo
+#
+#
